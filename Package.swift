c30908f744516f794aed588446183b18af44912e
Brian Strobach <brian@appsaurus.io>
2019-05-01 11:25:31 -0500

    Swift 5 running on Linux and macOS

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:4.0
+// swift-tools-version:5.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -10,12 +10,12 @@ let package = Package(
     ],
     dependencies: [
 		//Appsaurus Packages
-		.package(url: "https://github.com/Appsaurus/VaporTestUtils", .upToNextMajor(from: "0.1.0")),
-		.package(url: "https://github.com/Appsaurus/CodableExtensions", .upToNextMajor(from: "1.0.0")),
+		.package(url: "https://github.com/Appsaurus/VaporTestUtils", from: "0.1.0"),
+		.package(url: "https://github.com/Appsaurus/CodableExtensions", from: "1.0.0"),
 
 		//Vapor Packages
 		.package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
-		.package(url: "https://github.com/vapor/fluent.git", .upToNextMajor(from:"3.0.0")),
+		.package(url: "https://github.com/vapor/fluent.git", from:"3.0.0"),
     ],
     targets: [
         .target(name: "FluentTestUtils", dependencies: ["Vapor", "Fluent", "VaporTestUtils", "CodableExtensions"]),
